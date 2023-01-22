4b21054c8e28eebad295d1b818d6530bc57b4ddc
Jemima <contatojemima@gmail.com>
2023-01-22 18:15:18 -0300

    Aula 09 - Marcação do formulário
    - Add form into App( )
    - install phosphor-react library

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,29 @@
 import todoLogo from './assets/todo-logo.svg'
 import './App.css'
+import { PlusCircle } from 'phosphor-react'
 
 export function App() {
+  function addTask() {
+    console.log('Task criada!')
+  }
+
   return (
     <div className="App">
-      Eai
       <div className="wrapper">
         <header>
           <img src={todoLogo} alt="Logo todo list" />
         </header>
+
+        <main>
+          <form>
+            <input type="text" placeholder='Adicione uma nova tarefa' />
+            <button type="submit" onClick={addTask} >
+              Criar <PlusCircle size={32} />
+            </button>
+          </form>
+        </main>
       </div>
     </div>
+
   )
 }
