012217c8f105ea865ef8d27c9346bb7bc7a16ae7
Jemima <contatojemima@gmail.com>
2023-01-22 18:03:02 -0300

    Aula 08 - Marcação do header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
+import todoLogo from './assets/todo-logo.svg'
 import './App.css'
 
 export function App() {
   return (
     <div className="App">
       Eai
+      <div className="wrapper">
+        <header>
+          <img src={todoLogo} alt="Logo todo list" />
+        </header>
+      </div>
     </div>
   )
 }
