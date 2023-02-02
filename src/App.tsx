5dc83be7c51a6d37aab7b7ed7af24c7fcac0c407
Jemima <contatojemima@gmail.com>
2023-02-02 23:25:23 +0000

    Aula 21 - Checkpoints no código

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,29 @@ import styles from './styles/App.module.css'
 import { CheckCircle, PlusCircle, Trash } from 'phosphor-react'
 
 export function App() {
-  function addTask() {
+  function addTask(event: any) {
+    event.preventDefault()
     console.log('Task criada!')
+  }
+
+  function deleteTask() {
+    console.log('Task excluída!')
+  }
+
+  function toggleTask() {
+    console.log('Task marcada/desmarcada!')
   }
 
   return (
     <div className="App">
       <div className={styles.wrapper} >
+        {/* Header */}
         <header className={styles.header} >
           <img src={todoLogo} alt="Logo todo list" />
         </header>
 
         <main>
+          {/* NewTaskForm */}
           <form className={styles.new_task_form}>
             <input type="text" placeholder='Adicione uma nova tarefa' />
             <button type="submit" onClick={addTask} >
@@ -23,6 +34,7 @@ export function App() {
             </button>
           </form>
 
+          {/* TaskCounter */}
           <div className={styles.tasks_counter} >
             <strong className={styles.all_tasks} >
               Tarefas criadas <span className={styles.count} >0</span>
@@ -33,6 +45,7 @@ export function App() {
             </strong>
           </div>
 
+          {/* EmptyTask */}
           <div className={styles.empty_tasks}>
             <img src={emptyTasks} alt="" />
             <div>
@@ -45,11 +58,11 @@ export function App() {
             {/* TASK  */}
             <div className={styles.task}>
               <div>
-                <CheckCircle className={styles.checked_btn} size={28} weight="duotone" />
+                <CheckCircle onClick={toggleTask} className={styles.checked_btn} size={28} weight="duotone" />
                 {/* <Circle className={styles.check_btn} size={28} weight="duotone" /> */}
                 <p className={styles.risked} >Dar banho nos gatos</p>
               </div>
-              <Trash className={styles.delete_btn} size={24} />
+              <Trash onClick={deleteTask} className={styles.delete_btn} size={24} />
             </div>
           </div>
         </main>
